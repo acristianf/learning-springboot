4f0354cdc06698dc6d1e075bdd02a277742d1507
acristianf <acristianf@hotmail.com>
2023-07-04 20:22:22 -0300

    Service component and App context.

---
+++ src/main/java/com/cristian/lil/learningspring/business/ReservationService.java
@@ -0,0 +1,49 @@
+package com.cristian.lil.learningspring.business;
+
+import com.cristian.lil.learningspring.data.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ReservationService {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<RoomReservation> getRoomReservationForDate(Date date) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getRoomNumber());
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+        Iterable<Reservation> reservations = this.reservationRepository.findReservationByResDate(new java.sql.Date(date.getTime()));
+        reservations.forEach(reservation -> {
+            RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
+            roomReservation.setDate(date);
+            Guest guest = this.guestRepository.findById(reservation.getGuestId()).get();
+            roomReservation.setFirstName(guest.getFirstName());
+            roomReservation.setGuestId(guest.getId());
+            roomReservation.setLastName(guest.getLastName());
+        });
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        roomReservationMap.forEach((id, roomReservation) -> {
+            if (roomReservation.getDate() == date) {
+                roomReservations.add(roomReservation);
+            }
+        });
+        return roomReservations;
+    }
+}
