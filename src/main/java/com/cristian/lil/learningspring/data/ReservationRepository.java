f90459122e8c187939b7e66fe223a99ca9450266
acristianf <acristianf@hotmail.com>
2023-07-04 19:12:51 -0300

    Database connection, entities and repository creation.

---
+++ src/main/java/com/cristian/lil/learningspring/data/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.cristian.lil.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByResDate(Date date);
+}
