41bd9114f0e162b2c6e5241521e6499b8ddc2bed
acristianf <acristianf@hotmail.com>
2023-07-05 11:40:19 -0300

    Show guests ordered by last name.

--- src/main/java/com/cristian/lil/learningspring/web/GuestController.java
+++ src/main/java/com/cristian/lil/learningspring/web/GuestController.java
@@ -21,6 +21,12 @@ public class GuestController {
     @RequestMapping(method = RequestMethod.GET)
     public String getGuests(Model model) {
         List<Guest> guestList = (List<Guest>) guestRepository.findAll();
+        guestList.sort((g1, g2) -> {
+            if (g1.getLastName().equals(g2.getLastName())) {
+                return g1.getFirstName().compareTo(g2.getFirstName());
+            }
+            return g1.getLastName().compareTo(g2.getLastName());
+        });
         model.addAttribute("guestList", guestList);
         return "guests";
     }
